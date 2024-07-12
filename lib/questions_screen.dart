0a222de619e8c34404074010c325a71adc4db3c3
Jaroslav Solocha <jaroslav.solocha@gmail.com>
2024-07-12 18:55:20 +0200

    working with data

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +14,27 @@ class QuestionsScreen extends StatefulWidget {
 class _Queztions extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen ');
+    var currentQuestion = questions[0];
+
+    questions[0].text;
+    //print(currentQuestion);
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            questions[0].text, // ? why does not work ?
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
